1581616775cebcfe39b435363ee1d70ae43a9d38
Pedro Oliveira <>
2023-07-04 15:26:50 +0100

    feat(movie list): Add overview text.

--- velv_challenge/thortful_challenge/App/Scenes/Movie Details/Presentation/MovieDetailsViewModel.swift
+++ velv_challenge/thortful_challenge/App/Scenes/Movie Details/Presentation/MovieDetailsViewModel.swift
@@ -10,15 +10,18 @@ import Foundation
 struct MovieDetailsViewModel {
 
     private let movie: Movie
+
     private weak var coordinator: Coordinator?
 
     let posterURLString: String?
+    let overviewText: String
 
     init(
         movie: Movie,
         coordinator: Coordinator? = nil
     ) {
         self.movie = movie
+        overviewText = movie.overview
         self.coordinator = coordinator
 
         posterURLString = movie.buildPosterURLString()
